36e1861265bc216b271ab8b4ee67eaa1a7aadf0a
Danger <51889207+DangerDan88@users.noreply.github.com>
2022-09-05 22:20:54 -0600

    3d cube I am making

---
+++ 3d-cube/src/App.js
@@ -0,0 +1,23 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="container">
+        <div className="cube pers250">
+          <div className="face front">1</div>
+          <div className="face back">2</div>
+          <div className="face right">3 </div>
+          <div className="face left">4</div>
+          <div className="face top">5</div>
+          <div className="face bottom">6</div>
+        </div>
+      </div>
+      <p>3D-Cube</p>
+    </div>
+  );
+}
+/// need to study X and Y axis to get this cube to look 3D
+// why wont this fucking commit?
+export default App;
