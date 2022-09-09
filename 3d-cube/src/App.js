51924454fd3095e0a213e59f987a045bcfc83153
Danger <51889207+DangerDan88@users.noreply.github.com>
2022-09-09 10:15:38 -0600

    need to make a few sided more visible when spinning

--- 3d-cube/src/App.js
+++ 3d-cube/src/App.js
@@ -5,7 +5,7 @@ function App() {
     <div className="App">
       <div className="container">
         <div className="cube">
-          <div className="cube__face cube__face--front">1</div>
+          <div className="cube__face cube__face--front"></div>
           <div className="cube__face cube__face--back"></div>
           <div className="cube__face cube__face--right">3 </div>
           <div className="cube__face cube__face--left">4</div>
@@ -18,4 +18,5 @@ function App() {
 }
 //The perspective is what gives us the 3D impression. The farther from the viewer the elements are, the smaller they are.
 // need to figure out how to rotate it
+// might need a js function to get this cube to rotate correctly
 export default App;
