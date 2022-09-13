5c42d274ee95caaf8865d42f132e2ed83bcf8d61
Danger <51889207+DangerDan88@users.noreply.github.com>
2022-09-13 10:10:10 -0600

    added a few more images not set on them or the cube quite yet

--- 3d-cube/src/App.js
+++ 3d-cube/src/App.js
@@ -3,13 +3,14 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
+      {" "}
       <div className="container">
         <div className="cube">
           <div className="cube__face cube__face--front"></div>
           <div className="cube__face cube__face--back"></div>
-          <div className="cube__face cube__face--right">3 </div>
-          <div className="cube__face cube__face--left">4</div>
-          <div className="cube__face cube__face--top">5</div>
+          <div className="cube__face cube__face--right"> </div>
+          <div className="cube__face cube__face--left"></div>
+          <div className="cube__face cube__face--top"></div>
           <div className="cube__face cube__face--bottom"></div>
         </div>
       </div>
@@ -17,4 +18,5 @@ function App() {
   );
 }
 // need to figure out how to get a few sides to show a little more
+// see if I can make top of cube visible on start
 export default App;
